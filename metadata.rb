26ed487d80dac179a770412aa27402e952b1c5d2
David Montgomery <davidmontgomery@gmail.com>
2016-01-11 19:27:27 +0800

    f

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             "mysql"
+maintainer       "Rtb"
+maintainer_email "nobdy"
+license          "Apache 2.0"
+description      "Installs mysql"
+version          "1.0.0"
+%w{ubuntu}.each do |os|
+  supports os
+end
