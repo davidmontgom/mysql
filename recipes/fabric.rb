ab29140e5cfb8341f016bff3b8c427b57ac0c532
David Montgomery <davidmontgomery@gmail.com>
2016-01-12 08:15:36 +0800

    update

--- recipes/fabric.rb
+++ recipes/fabric.rb
@@ -97,7 +97,7 @@ Fabric will be manually maitained
 
 n
 
-
+https://dev.mysql.com/doc/mysql-utilities/1.5/en/connector-python-fabric.html
 http://dev.mysql.com/doc/mysql-utilities/1.5/en/fabric-faq.html
 https://blogs.oracle.com/jbalint/entry/accessing_fabric_ha_groups_from
 https://dev.mysql.com/tech-resources/articles/mysql-fabric-ga.html
