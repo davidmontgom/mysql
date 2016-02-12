9e2bc43038da5cbf5747a92bba1cfa157e0a37f7
David Montgomery <davidmontgomery@gmail.com>
2016-02-12 16:38:28 +0800

    update

--- recipes/client.rb
+++ recipes/client.rb
@@ -1,8 +1,8 @@
-remote_file "mysql-connector-python_2.1.3-1ubuntu14.04_all.deb" do
+remote_file "#{Chef::Config[:file_cache_path]}/mysql-connector-python_2.1.3-1ubuntu14.04_all.deb" do
     source "https://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python_2.1.3-1ubuntu14.04_all.deb"
     action :create_if_missing
 end
 
-dpkg_package "mysql-connector-python_2.1.3-1ubuntu14.04_all.deb" do
+dpkg_package "#{Chef::Config[:file_cache_path]}/mysql-connector-python_2.1.3-1ubuntu14.04_all.deb" do
   action :install
 end 
