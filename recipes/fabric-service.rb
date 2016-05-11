66b8dcf2f63331e58aba049cc2d33932e1d15403
David Montgomery <davidmontgomery@gmail.com>
2016-05-11 22:50:29 +0800

    update

--- recipes/fabric-service.rb
+++ recipes/fabric-service.rb
@@ -9,7 +9,16 @@ easy_install_package "boto" do
   action :install
 end
 
+package "libffi-dev" do
+  action :install
+end
+
+package "libssl-dev" do
+  action :install
+end
+
 easy_install_package "paramiko" do
+  options "-U"
   action :install
 end
 
